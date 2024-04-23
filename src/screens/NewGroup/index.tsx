bc006c79aafe32f4e704eb7d2ab88cfc9cbf89ea
fernando-henrique <fernandohsferreira@gmail.com>
2024-04-22 23:22:14 -0400

    rotas finalizadas

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,11 +4,13 @@ import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup(){
+    const [group, setGroup] = useState('');
     const navigation = useNavigation();
     function handleNew(){
-        navigation.navigate('players')
+        navigation.navigate('players', {group})
     }
     return(
         <Container>
@@ -21,6 +23,7 @@ export function NewGroup(){
                 />
                 <Input 
                     placeholder="Nome da turma"
+                    onChangeText={setGroup}
                 />
                 <Button
                     title='Criar'
