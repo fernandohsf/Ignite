9154cff1167812ed630400a067e769b3aafcea28
fernando-henrique <fernandohsferreira@gmail.com>
2024-04-22 21:52:02 -0400

    Finalização do front end

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,38 @@
+import { useState } from 'react';
 import { Container } from './styles';
 import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
 import { Highlight } from '@components/Highlight';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
       <Highlight
         title = 'Turmas'
         subtitle='Jogue com a sua turma'
+      />
+      <FlatList 
+        data = {groups}
+        keyExtractor={item => item}
+        renderItem={({item})=> (
+          <GroupCard 
+            title= {item}
+
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex:1}}
+        ListEmptyComponent={()=> (
+          <ListEmpty message='Que tal cadastrar a primeira turma?' />
+      )}
+      />
+      <Button 
+        title = 'Criar nova turma'
       />
     </Container>
   );
